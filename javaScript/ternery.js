43190d13c54e1e65d79f543dfcdd66d7e8731885
syedomer17 <syedomerali2006@gmail.com>
2025-01-28 19:33:36 +0530

    ternery

---
+++ javaScript/ternery.js
@@ -0,0 +1,7 @@
+let age = 18;
+let message = age >= 18 ? "You are an adult" : "You are a minor";
+console.log(message);
+
+let score = 85;
+let grade = score > 90 ? "A" : score > 75 ? "B" : "C";
+console.log(grade);
