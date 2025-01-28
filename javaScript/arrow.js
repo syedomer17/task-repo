d7666248e065e1907e71cc15daf9b78a2a706486
syedomer17 <syedomerali2006@gmail.com>
2025-01-28 19:25:02 +0530

    arrow-Functions

---
+++ javaScript/arrow.js
@@ -0,0 +1,12 @@
+const sum = (a,b)=>{
+ let result =  console.log(a+b);
+ console.log(result); 
+ return;
+}
+sum(3,4)
+
+const square = (x) => x * x;
+console.log(square(4)); 
+
+const greet = () => console.log("Hello!");
+greet();
